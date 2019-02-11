4bf682eac373ab5acdb1f51d78e11ee8e5f1e36f
Will Jones <williamjones1.wj@gmail.com>
2019-02-10 19:51:39 -0700

    last save 2/10

--- app/game.js
+++ app/game.js
@@ -1,3 +1,11 @@
+/* to do
+-edit tip and pop like mlady to allow for double damage 
+-correct defeat / vicotry messages
+-add dorito image swaps to reset function (set disabled to false)
+-add instructions to default page load spans
+-add message to reset spans
+*/
+
 let enemyHealth = 100;
 let playerHealth = 100;
 
@@ -5,30 +13,53 @@ let playerDoritosActive = false
 let enemyDoritosActive = false
 
 function tipFedora() {
+    let damageAmount = 5;
     if (enemyHealth != 0 && enemyHealth > 0) {
-        enemyHealth = enemyHealth - 5
+        if (playerDoritosActive ==true){
+            damageAmount = damageAmount *2;
+        }
+        enemyHealth= enemyHealth -damageAmount;
     }
     enemyCounter()
     drawHealth()
     console.log('Current Enemy Health =', + enemyHealth)
     console.log('Current Player Health =', + playerHealth)
-
-    document.getElementById('previousAction').innerHTML = 'You tipped your fedora dealing 5 damage'
-    document.getElementById('previousActionImage').innerHTML = '<img src="./assets/tipgif.gif">'
+    if (playerHealth >0 && enemyHealth >0)
+    {
+        if (playerDoritosActive) {
+        document.getElementById('previousActionImage').innerHTML = '<img src="./assets/tipgif.gif">'
+        playerDoritosActive = false;
+    } else {
+        document.getElementById('previousActionImage').innerHTML = '<img src="./assets/tipgif.gif">'
+    }
+    document.getElementById('previousAction').innerHTML = `You tipped your fedora dealing ${damageAmount} damage`
     document.getElementById('moreInfo').innerHTML = 'Enemy has counterattacked!'
+    }
 }
 
 function popCollar() {
+    let damageAmount = 10;
     if (enemyHealth != 0 && enemyHealth > 0) {
-        enemyHealth = enemyHealth - 10
+        if (playerDoritosActive== true){
+            damageAmount = damageAmount * 2;
+        }
+        enemyHealth = enemyHealth -damageAmount;
     }
     enemyCounter()
     drawHealth()
     console.log('Current Enemy Health =', + enemyHealth)
     console.log('Current Player Health =', + playerHealth)
-    document.getElementById('previousAction').innerHTML = 'You popped your trenchcoat collar dealing 10 damage'
-    document.getElementById('previousActionImage').innerHTML = '<img src="./assets/collar.gif">'
+    if (playerHealth >0 && enemyHealth >0)
+    {
+        if (playerDoritosActive) {
+        document.getElementById('previousActionImage').innerHTML = '<img src="./assets/collar.gif">'
+        playerDoritosActive = false;
+    } else {
+        document.getElementById('previousActionImage').innerHTML = '<img src="./assets/collar.gif">'
+    }
+    document.getElementById('previousAction').innerHTML = `You popped your trenchcoat collar dealing ${damageAmount} damage`
     document.getElementById('moreInfo').innerHTML = 'Enemy has counterattacked!'
+    }
 }
 
 function dropMlady() {
@@ -43,7 +74,6 @@ function dropMlady() {
     drawHealth()
     console.log('Current Enemy Health =', + enemyHealth)
     console.log('Current Player Health =', + playerHealth)
-    console.log('setting mlady')
     if (playerHealth >0 && enemyHealth >0)
     {
         if (playerDoritosActive) {
@@ -91,45 +121,55 @@ function drinkDew2() {
 function eatChip1(){
 playerDoritosActive = true;
 document.getElementById('playerChip').disabled = true;
-document.getElementById('playerChip').innerHTML = `<img src="./assets/doritoshot.png">`
+document.getElementById('playerChip').opinnerHTML = `<img src="./assets/doritoshot.png">`
 document.getElementById('previousAction').innerHTML = ' DORITO DOUBLE DAMAGE ACTIVE FOR NEXT ATTACK!';
+document.getElementById('previousActionImage').innerHTML = `<img src="./assets/2x.png">`;
+document.getElementById('moreInfo').innerHTML = '';
 }
 function eatChip2(){
 enemyDoritosActive = true;
 document.getElementById('enemyChip').disabled = true;
 document.getElementById('enemyChip').innerHTML = `<img src="./assets/doritoshot.png">`
 document.getElementById('previousAction').innerHTML = ' DORITO DOUBLE DAMAGE ACTIVE FOR NEXT ATTACK!';
+document.getElementById('previousActionImage').innerHTML = `<img src="./assets/2x.png">`;
+document.getElementById('moreInfo').innerHTML = '';
 }
 
 function drawKatana1() {
     let katanaSwing = Math.random();
     if (katanaSwing < .49) {
         playerHealth = 0;
+        drawHealth()
         document.getElementById('previousActionImage').innerHTML = '<img src="./assets/katanalose.gif">'
         document.getElementById('moreInfo').innerHTML = 'Your lack of skill has resulted in fatal self harm'
     } else if (katanaSwing > .51) {
         enemyHealth = 0;
+        drawHealth()
         document.getElementById('previousActionImage').innerHTML = '<img src="./assets/katanawin.gif">'
         document.getElementById('moreInfo').innerHTML = 'YOUR POWER LEVEL IS OVER 9000'
+        
     }
     console.log(playerHealth)
     console.log(enemyHealth)
-    drawHealth()
+    // drawHealth()
+    // document.getElementById('previousActionImage').innerHTML = '<img src="./assets/katanawin.gif">'
+    // document.getElementById('moreInfo').innerHTML = 'YOUR POWER LEVEL IS OVER 9000'
 }
 function drawKatana2() {
     let katanaSwing = Math.random();
     if (katanaSwing < .49) {
         enemyHealth = 0;
+        drawHealth()
         document.getElementById('previousActionImage').innerHTML = '<img src="./assets/katanawin.gif">'
         document.getElementById('moreInfo').innerHTML = 'Your mind games have caused your opponent to destroy himself'
     } else if (katanaSwing > .51) {
         playerHealth = 0;
+        drawHealth()
         document.getElementById('previousActionImage').innerHTML = '<img src="./assets/katanalose.gif">'
         document.getElementById('moreInfo').innerHTML = 'Why did you allow your opponent to draw his blade?'
     }
     console.log(playerHealth)
     console.log(enemyHealth)
-    drawHealth()
 }
 
 function resetButton() {
@@ -139,9 +179,13 @@ function resetButton() {
     if (playerHealth < 100) {
         playerHealth = 100
     }
-    document.getElementById('previousAction').innerHTML = '';
-    document.getElementById('previousActionImage').innerHTML = '';
-    document.getElementById('moreInfo').innerHTML = '';
+    document.getElementById('previousAction').innerHTML = ' tip: Doritos can only be used once per game';
+    document.getElementById('previousActionImage').innerHTML = 'tip: Katana can one shot, both the enemy and yourself';
+    document.getElementById('moreInfo').innerHTML = 'tip: Mtn. Dew can be used multiple times ';
+    document.getElementById('playerChip').disabled = false;
+    document.getElementById('enemyChip').disabled = false;
+    document.getElementById('playerChip').innerHTML = `<img src="./assets/doritos.png">`
+    document.getElementById('enemyChip').innerHTML = `<img src="./assets/doritos.png">`
     drawHealth()
     console.log('Current Player Health =', + playerHealth)
     console.log('Current Enemy Health =', + enemyHealth)
@@ -158,17 +202,19 @@ function drawHealth() {
     if (enemyHealth <= 0 && playerHealth > 0) {
         console.log('victory was achieved, updating the image')
         document.getElementById('previousAction').innerHTML = 'The White Knight is Victorious';
-        document.getElementById('previousActionImage').innerHTML = '';
+        document.getElementById('previousActionImage').innerHTML =  `<img src="./assets/winning.gif">`;
         document.getElementById('moreInfo').innerHTML = '';
     } else if (playerHealth <= 0 && enemyHealth > 0) {
         document.getElementById('previousAction').innerHTML = 'Darkness Hath Prevailed, Your enemy is victorious';
-        document.getElementById('previousActionImage').innerHTML = '';
+        document.getElementById('previousActionImage').innerHTML =  `<img src="./assets/lose.gif">`;
         document.getElementById('moreInfo').innerHTML = '';
+    } else if(playerHealth <= 0 && enemyHealth <= 0){
+        document.getElementById('previousAction').innerHTML = 'You are both dead...';
+        document.getElementById('previousActionImage').innerHTML = `<img src="./assets/oops.gif">`;
+        document.getElementById('moreInfo').innerHTML = '...Great Job';
+
     }
 
 }
 drawHealth()
 
-
-// if (enemyHealth<=0){
-//     alert('OK YOU GOT HIM... Jeez he is at ' + enemyHealth  + ' health mlord')
